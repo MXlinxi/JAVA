46e73c6606e0643e731509618d57043ffaa19505
小林溪溪 <2082693127@qq.com>
2020-11-15 13:13:57 +0800

    定义变量与简单数据类型之间的转换

--- src/Demo5.java
+++ src/Demo5.java
@@ -1,7 +1,43 @@
 /**
- *String类型与基本数据类型之间的转换
- * 1.
+ * String类型与基本数据类型之间的转换
  */
 public class Demo5 {
+    public static void main(String[] args) {
+        //基本数据类型转String类型
+        int a = 23;
+        String b = a + "";
+        System.out.println(b.length());
 
+        //String类型转int
+        String s = "1";
+        int c = Integer.parseInt(s);
+        //String类型转double
+        double d = Double.parseDouble(s);
+        //String类型转byte
+        byte e = Byte.parseByte(s);
+        //String类型转short
+        short f = Short.parseShort(s);
+        //String类型转long
+        long g = Long.parseLong(s);
+        //String类型转float
+        float h = Float.parseFloat(s);
+        //String类型转boolean,非true字符串都转为false
+        boolean i = Boolean.parseBoolean(s);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+
+        //String类型转char类型
+        String str = "hello";
+        char ss = str.charAt(0);
+        System.out.println(ss);
+        //char类型参与运算
+        System.out.println('a' + 1 + "hello");
+    }
 }
---
+++ src/Demo6.java
@@ -0,0 +1,24 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * 运算符
+ */
+public class Demo6 {
+    public static void main(String[] args) {
+        System.out.println(-10/3);
+        //取余 a-a/b*b
+        System.out.println(-10%3);
+        System.out.println(2.3%-2);
+        //自增自减运算符
+        int a=4;
+        int b=a++;
+        System.out.println(a+b);
+        System.out.println(a++);
+        System.out.println(++a);
+        //关系运算符 结果为boolean类型
+        boolean flag=a!=b;
+        System.out.println(flag);
+        //逻辑运算符 短路与&& 逻辑与&
+
+    }
+}
