03e0d4e4ab6cd46dec8c5d2512b8e19c03831d90
小林溪溪 <2082693127@qq.com>
2020-11-18 21:53:07 +0800

    switch-case语法结构

--- src/Demo10.java
+++ src/Demo10.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
 public class Demo10 {
     public static void main(String[] args) {
         //例 变量参与运算作为判断，其本身的值也会改变
-        boolean b1=true;
-        boolean b2=false;
-        if((b1=2>3)&&(b2=5>4)){
+        boolean b1 = true;
+        boolean b2 = false;
+        if ((b1 = 2 > 3) && (b2 = 5 > 4)) {
             System.out.println("hello");
-        }else{
-            System.out.println("b1="+b1+";b2="+b2);
+        } else {
+            System.out.println("b1=" + b1 + ";b2=" + b2);
         }
         Scanner input = new Scanner(System.in);
         /**
