55c432b2c0151c1339e925b7b699e9c0010b18b1
小林溪溪 <2082693127@qq.com>
2020-11-19 00:36:05 +0800

    打印一定范围内的整数公式

---
+++ src/Demo11.java
@@ -0,0 +1,11 @@
+/**
+ * 打印随机数
+ */
+public class Demo11 {
+    public static void main(String[] args) {
+        //产生[0,1)范围的随机数
+        double d=Math.random();
+        //产生[a,b)的整数
+        //int x=(int)(Math.random()*(b-a+1)+a)
+    }
+}
