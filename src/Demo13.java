2030c9c030e5f4ef6d2eca33a75f08b77f7c3320
小林溪溪 <2082693127@qq.com>
2020-12-03 20:47:01 +0800

    while循环题型2

--- src/Demo13.java
+++ src/Demo13.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  */
 public class Demo13 {
     public static void main(String[] args) {
-        //不断输入姓名，直到输入”q“为止
+        //案例：不断输入姓名，直到输入”q“为止
         Scanner input = new Scanner(System.in);
 /*        while (true) {
             System.out.print("请输入姓名：");
@@ -19,7 +19,7 @@ public class Demo13 {
             System.out.print("请输入姓名:");
             name = input.next();
         }
-        //不断输入月份，直到输入错误为止
+        //案例：不断输入月份，直到输入错误为止
         int month = 1;
         while (month > 0 && month < 11) {
             System.out.print("请输入月份：");
