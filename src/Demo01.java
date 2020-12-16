ae09f0503bc20254116d1f6c43e9e3c69a7b31f4
小林溪溪 <2082693127@qq.com>
2020-12-16 19:35:41 +0800

    java注释

--- src/Demo01.java
+++ src/Demo01.java
@@ -4,7 +4,8 @@
  * 3.转义字符 \t \n \\ \' \" \r（定义到当前行首） 一个字符
  * 4.四种注释 文档注释可以生成帮助文档
  */
-public class Demo1 {
+
+public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello,wo\rrld!");
     }
