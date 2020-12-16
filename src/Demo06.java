0d0cea66014211fd1750943d83d9d79893af8445
小林溪溪 <2082693127@qq.com>
2020-12-16 19:50:49 +0800

    java运算符

--- src/Demo06.java
+++ src/Demo06.java
@@ -4,7 +4,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
  * 运算符
  * 优先级：小括号>自增/自减运算符/逻辑非>算术运算符>关系运算符>逻辑运算符>三元运算符>赋值运算符
  */
-public class Demo6 {
+public class Demo06 {
     public static void main(String[] args) {
         System.out.println(-10 / 3);
         //取余 a-(int)a/b*b
