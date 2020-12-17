e8eb676a15ada28362d72664d0faf4af6fbd4805
小林溪溪 <2082693127@qq.com>
2020-12-17 20:46:00 +0800

    嵌套循环案例

--- src/Demo16.java
+++ src/Demo16.java
@@ -7,33 +7,67 @@ public class Demo16 {
     public static void main(String[] args) {
 
         //案例：打印空心平行四边形
+        int row = 10;
+        for (int i = 1; i < row; i++) {
+            //打印空格
+            for (int j = 1; j < i; j++) {
+                System.out.print("\t ");
+            }
+            //打印*
+            for (int j = 1; j < row; j++) {
+                if (j == 1 || j == row - 1 || i == 1 || i == row - 1)
+                    System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
 
         //案例：打印等腰三角形
+        for (int i = 1; i < row; i++) {
+            //打印空格
+            for (int j = 0; j < row - i; j++) {
+                System.out.print(" ");
+            }
+            //打印*
+            for (int j = 1; j < 2 * i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-
-        //案例：倒等腰三角形
-
+        //案例：打印倒等腰三角形
+        for (int i = row; i > 0; i--) {
+            //打印空格
+            for (int j = row; j > i; j--) {
+                System.out.print(" ");
+            }
+            //打印*
+            for (int j = 1; j < 2 * i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         //案例：打印正方形
-        for(int i=1;i<5;i++){
-            for(int j=1;j<5;j++){
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
                 System.out.print("\t*");
             }
             System.out.println();
         }
 
         //案例：打印直角三角形
-        for(int i=1;i<8;i++){
-            for(int j=1;j<=i;j++) {
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("\t*");
             }
             System.out.println();
         }
 
         //案例：打印乘法表
-        for (int i=1;i<10;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("\t"+i+"*"+j+"="+i*j);
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("\t" + i + "*" + j + "=" + i * j);
             }
             System.out.println();
         }
