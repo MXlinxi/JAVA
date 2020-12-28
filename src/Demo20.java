174aa910549af344a2abeba3fe4d4222640c61af
小林溪溪 <2082693127@qq.com>
2020-12-29 00:18:00 +0800

    数组

---
+++ src/Demo20.java
@@ -0,0 +1,17 @@
+/**
+ * 数组
+ * 1.数组的声明
+ * 2.数组的应用
+ */
+public class Demo20 {
+    public static void main(String[] args) {
+        //数组声明,数组是一种引用类型
+        int[] array;
+        //开辟空间，必须声明数组长度
+        array = new int[5];
+        //通过下标手动赋值，有默认值
+        array[0] = 6;
+        //通过下标访问，注意数组越界异常（属于运行时异常）
+        System.out.println(array[0]);
+    }
+}
